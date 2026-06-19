6d5b14cf2ebd9fa5605660214658c36e1bc22a44
Jon Davis <jldavisjr@yahoo.com>
2026-06-18 23:02:15 -0400

    Finally fixed u128 multiplication. Way more painful than was reasonable. Last bug was a typo swapping a for b.

--- main.cpp
+++ main.cpp
@@ -3,18 +3,108 @@
 #include "u128.h"
 #include "LCG.h"
 
+uint8_t mul8_hi(uint8_t a, uint8_t b){
+    
+    uint8_t a0 = (a&0x0FU);
+    uint8_t b0 = (b&0x0FU);
+    uint8_t a1 = (a&0xF0U)>>4;
+    uint8_t b1 = (b&0xF0U)>>4;
+    
+    uint8_t lo = (a0*b0)&0xFF;
+    uint8_t hi = (a1*b1)&0xFF;
+    uint8_t ma = (a1*b0)&0xFF;
+    uint8_t mb = (a0*b1)&0xFF;
+    
+    uint8_t carry = ( (ma&0xF)+(mb&0xF)+(lo>>4))>>4;
+    
+    return (hi+(ma>>4)+(mb>>4)+carry)&0xFF;
+}
+
+struct u16{
+    uint8_t x1,x0;
+    
+    friend u16 operator + (u16 a, u16 b){
+        u16 c{};
+        
+        c.x0 = a.x0+b.x0;
+        c.x1 = a.x1+b.x1;
+        
+        // overflow low to high
+        if (c.x0 < a.x0)
+            c.x1++;
+        
+        return c;
+    }
+    
+    friend u16 operator * (u16 a, u16 b){
+        u16 c{};
+        
+        uint8_t a0 = a.x0;
+        uint8_t b0 = b.x0;
+        uint8_t a1 = a.x1;
+        uint8_t b1 = b.x1;
+        
+        u16 lo = {mul8_hi(a0,b0),uint8_t(a0*b0)};
+        u16 ma = {uint8_t(a1*b0),0};
+        u16 mb = {uint8_t(a0*b1),0};
+        
+        c = lo+ma+mb;
+        
+        return c;
+    }
+};
+
 int main(){
     /*
-    u128 a{0xFFFF'FFFF'FFFF'FFFF,0xFFFF'FFFF'FFFF'FFFF},b{0xFFFF'FFFF'FFFF'FFFF,0xFFFF'FFFF'FFFF'FFFF},c{};
+    uint8_t a = 0x0B, b = 0xDA;
+    
+    uint16_t res = a*b;
+    uint8_t lo = a*b;
+    uint8_t hi = mul8_hi(a,b);
+    
+    std::cout << std::hex << int(a) << "*" << int(b) << " = ";
+    std::cout << std::hex << res << ":";
+    std::cout << std::hex << int(hi) << "'" << int(lo) << "\n";
+    
+    return 0;//*/
+    
+    /*
+    
+    uint16_t huh = uint16_t( uint64_t(0xFFFF)*uint64_t(0xFFFF) );
+    int bad_count = 0;
+    for(uint32_t x = 0; x <= 0xFFFF; x++){
+        for(uint32_t y = 0; y <= 0xFFFF; y++){
+            
+            uint16_t a = x, b = y;
+            
+            u16 A{uint8_t(a>>8),uint8_t(a)},B{uint8_t(b>>8),uint8_t(b)};
+            uint16_t c = uint64_t(a)*uint64_t(b);
+            u16 C = A*B;
+            
+            uint16_t myC = (uint16_t(C.x1)<<8)|C.x0;
+            
+            if(c!=myC) bad_count++;
+        }
+        if( (x%0x0800)==0 ) std::cout << std::hex << "progress: " << bad_count << " failures so far.\n";
+    }
+    
+    std::cout << "bad count:" << bad_count << "\n";//*/
+    
+    //*
+    //u128 a{0xFFFF'FFFF'FFFF'FFFF,0xFFFF'FFFF'FFFF'FFFF},b{0xFFFF'FFFF'FFFF'FFFF,0xFFFF'FFFF'FFFF'FFFF},c{0,0};
+    //u128 a{0,0},b{0,0},c{0,0};
+    u128 a{0xFFFF'FFFF'FFFF'FFFF,0xFFFF'AAAA'AAAA'AAAA},b{0xFFFF'FFFF'FFFF'FFFF,0xFFFFFFFFFFFF7},c{0,0};
     c = a*b;
     
+    std::cout << std::hex << "0x" << std::setw(16) << std::setfill('0') << a.x1 << "'"  << std::setw(16) << std::setfill('0')<< a.x0 << "\n";
+    std::cout << std::hex << "0x" << std::setw(16) << std::setfill('0') << b.x1 << "'"  << std::setw(16) << std::setfill('0')<< b.x0 << "\n";
     std::cout << std::hex << "0x" << std::setw(16) << std::setfill('0') << c.x1 << "'"  << std::setw(16) << std::setfill('0')<< c.x0 << "\n";//*/
-    // terrible constants, lol. I'll search for better ones later.
+    /* terrible constants, lol. I'll search for better ones later.
     LCG<{0,5},{0,1}> my_first_lcg;
     for( int i = 0; i < 55; i++){
         std::cout << std::hex << "0x" << std::setw(16) << std::setfill('0') << my_first_lcg.next() << "\n";
         std::cout << std::hex << "0x" << std::setw(16) << std::setfill('0') << my_first_lcg.state.x1 << "'"  << std::setw(16) << std::setfill('0')<< my_first_lcg.state.x0 << "\n";
-    }
+    }//*/
     
 }
 
--- u128.h
+++ u128.h
@@ -1,67 +1,59 @@
 #pragma once
 
+#include <iostream>
+#include <iomanip>
+
+uint64_t mul64_hi(uint64_t a, uint64_t b){
+    
+    uint64_t a0 = (a&0xFFFF'FFFFU);
+    uint64_t b0 = (b&0xFFFF'FFFFU);
+    uint64_t a1 = (a>>32);
+    uint64_t b1 = (b>>32);
+    
+    uint64_t lo = a0*b0;
+    uint64_t hi = a1*b1;
+    uint64_t ma = a1*b0;
+    uint64_t mb = a0*b1;
+    
+    uint64_t carry = ( (0xFFFF'FFFFU&ma)+(0xFFFF'FFFFU&mb)+(lo>>32))>>32;
+    
+    return hi+(ma>>32)+(mb>>32)+carry;
+}
+
 struct u128{
     
     uint64_t x1 = 0, x0 = 0;
     
     uint64_t upper(){return x1;}
     
-    // not using karatsuba's. If this is ever "production" code, I should probably switch to a variant of it.
-    friend u128 operator * (u128 a, u128 b){
-        //u128 c{};
-        
-        /*// I gave up & just used the __uint128_t built in.
-        c.x0 = a.x0*b.x0;
-        
-        std::cout << std::hex << "0x" << std::setw(16) << std::setfill('0') << c.x0 << "\n";
-        
-        uint64_t    a0 = (a.x0&0xFFFF'FFFF),
-                    b0 = (b.x0&0xFFFF'FFFF),
-                    a1 = (a.x0>>32),
-                    b1 = (b.x0>>32),
-                    a2 = (a.x1&0xFFFF'FFFF),
-                    b2 = (b.x1&0xFFFF'FFFF),
-                    a3 = (a.x1>>32),
-                    b3 = (b.x1>>32);
-        // manual multiplication with 32 bit chunks. Keeps the overflow accurate.
-        c.x1 = a0*b0;
-        c.x1 >>= 32;
-        c.x1 += a1*b0;
-        c.x1 += a0*b1;
-        
-        c.x1 >>= 32;
-        
-        c.x1 += a0*b2;
-        c.x1 += a1*b1;
-        c.x1 += a2*b0;
-        
-        c.x1 += (a3*b0)<<32;
-        c.x1 += (a2*b1)<<32;
-        c.x1 += (a1*b2)<<32;
-        c.x1 += (a0*b3)<<32;//*/
-        
-        __uint128_t aa = __uint128_t(a.x1)<<64|__uint128_t(a.x0);
-        __uint128_t bb = __uint128_t(b.x1)<<64|__uint128_t(b.x0);
-        __uint128_t cc = aa*bb;
-        
-        // we can skip the last multiplication because it's out of bounds for a 128 bit multiplication.
-        
-        return {uint64_t((cc)>>64),(uint64_t)cc};
-    }
-    
     friend u128 operator + (u128 a, u128 b){
         u128 c{};
         
         c.x0 = a.x0+b.x0;
+        c.x1 = a.x1+b.x1;
         
-        uint64_t al = (a.x0&0xFFFF'FFFF),ah = (a.x0>>32),bl = (b.x0&0xFFFF'FFFF),bh = (b.x0>>32);
+        // overflow low to high
+        if (c.x0 < a.x0)
+            c.x1++;
         
-        c.x1 = al+bl;
-        c.x1 >>= 32;
-        c.x1 += ah+bh;
-        c.x1 >>= 32;
+        return c;
+    }
+    
+    // not using karatsuba's. If this is ever "production" code, I should probably switch to a variant of it.
+    friend u128 operator * (u128 a, u128 b){
+        u128 c{};
         
-        c.x1 += a.x1+b.x1;
+        uint64_t a0 = a.x0;
+        uint64_t b0 = b.x0;
+        uint64_t a1 = a.x1;
+        uint64_t b1 = b.x1;
+        
+        u128 lo,ma,mb;
+        lo = {mul64_hi(a0,b0),a0*b0};
+        ma = {a1*b0,0};
+        mb = {a0*b1,0};
+        
+        c = lo+ma+mb;
         
         return c;
     }
